073f21addf2a1e835bd23a4a716162ffd83c3390
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2018-07-29 08:11:23 -0500

    I completed the catData.  It has the name, img, and counter setup

---
+++ js/app.js
@@ -0,0 +1,28 @@
+//This the Cat Clicker data for the app.
+const catData = [
+    {   name: "fluffy",
+        img: "pic/christmas kitty.png",
+        clickNumber: 0
+    },
+    {
+        name: "furball",
+        img: "fluffy kitten.png",
+        clickNumber: 0
+    },
+    {
+        name: "sleepy",
+        img: "sleepy cat.png",
+        clickNumber: 0
+    },
+    {
+        name: "peppy",
+        img: "wakeup cat.png",
+        clickNumber: 0
+    }
+];
+
+//This is the catEngine which coordinates between the cat clicker model and the UI
+const catEngine = {
+    //This function will increment the counter
+    addCounter : function(){}
+};
